bf59c3ae563bf9183dd86392fac38c5d38f50d7f
jmichalenko <50920207+jmichalenko@users.noreply.github.com>
2021-01-22 15:21:54 -0500

    Update _init_.py

--- HelloNamev1/_init_.py
+++ HelloNamev1/_init_.py
@@ -3,6 +3,21 @@ import check50.c
 
 @check50.check()
 def exists():
-    """Hello exists"""
-    check50.exists("HelloName.c")
+    """hello.c exists"""
+    check50.exists("hello.c")
+
+@check50.check(exists)
+def compiles():
+    """hello.c compiles"""
+    check50.c.compile("hello.c", lcs50=True)
+
+@check50.check(compiles)
+def emma():
+    """responds to name Emma"""
+    check50.run("./hello").stdin("Emma").stdout("Emma").exit()
+
+@check50.check(compiles)
+def rodrigo():
+    """responds to name Rodrigo"""
+    check50.run("./hello").stdin("Rodrigo").stdout("Rodrigo").exit()
 
