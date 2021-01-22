ca895d35b397dfbbb798f93252845d2a3f75129b
jmichalenko <50920207+jmichalenko@users.noreply.github.com>
2021-01-22 15:41:04 -0500

    Update checks.py

--- HelloNamev1/checks.py
+++ HelloNamev1/checks.py
@@ -3,21 +3,21 @@ import check50.c
 
 @check50.check()
 def exists():
-    """hello.c exists"""
-    check50.exists("hello.c")
+    """HelloName.c exists"""
+    check50.exists("HelloName.c")
 
 @check50.check(exists)
 def compiles():
-    """hello.c compiles"""
-    check50.c.compile("hello.c", lcs50=True)
+    """HelloName.c compiles"""
+    check50.c.compile("HelloName.c", lcs50=True)
 
 @check50.check(compiles)
 def emma():
     """responds to name Emma"""
-    check50.run("./hello").stdin("Emma").stdout("Emma").exit()
+    check50.run("./HelloName").stdin("Emma").stdout("Hello, Emma!").exit()
 
 @check50.check(compiles)
 def rodrigo():
     """responds to name Rodrigo"""
-    check50.run("./hello").stdin("Rodrigo").stdout("Rodrigo").exit()
+    check50.run("./HelloName").stdin("Rodrigo").stdout("Hello, Rodrigo!").exit()
 
