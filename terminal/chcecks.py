d29abcd01cfeaf00308d7b32619ead387908d9c9
jmichalenko <50920207+jmichalenko@users.noreply.github.com>
2021-01-24 12:35:00 -0500

    Update chcecks.py

--- terminal/chcecks.py
+++ terminal/chcecks.py
@@ -4,10 +4,10 @@ import check50.c
 @check50.check()
 def exists():
     """apt1"""
-    check50.exists("./apt1/bathroom/bathroom_cabinet.txt")
+    check50.exists("/apt1/bathroom/bathroom_cabinet.txt")
 
 @check50.check()
 def exists():
     """apt1/bathroom exists"""
-    check50.exists("./apt1/kitchen/cabinet/cabinet_contents.txt")
+    check50.exists("/apt1/kitchen/cabinet/cabinet_contents.txt")
     
