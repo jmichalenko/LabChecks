08b6338ce00d9c94cdf4c5a1b936bada112788ad
jmichalenko <50920207+jmichalenko@users.noreply.github.com>
2021-01-25 12:35:12 -0500

    Update checks.py

--- terminal/checks.py
+++ terminal/checks.py
@@ -4,7 +4,7 @@ import check50.c
 @check50.check()
 def exists():
     """Kitchen_cabinet.txt exists"""
-    check50.exists("./kitchen/cabinet/cabinet_contents.txt")
+    check50.exists("./apt1/kitchen/cabinet/cabinet_contents.txt")
     
 @check50.check()
 def exists():
