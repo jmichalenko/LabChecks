78c20213d32321a029a71c36256d49cb7200c405
jmichalenko <50920207+jmichalenko@users.noreply.github.com>
2021-01-25 12:34:54 -0500

    Update checks.py

--- terminal/checks.py
+++ terminal/checks.py
@@ -3,8 +3,8 @@ import check50.c
 
 @check50.check()
 def exists():
-    """Bathroom_cabinet.txt exists"""
-    check50.exists("./bathroom/bathroom_cabinet/bathroom_cabinet.txt")
+    """Kitchen_cabinet.txt exists"""
+    check50.exists("./kitchen/cabinet/cabinet_contents.txt")
     
 @check50.check()
 def exists():
