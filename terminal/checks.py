41cf26cc60509289e0cfea573a8d42b8532ef688
jmichalenko <50920207+jmichalenko@users.noreply.github.com>
2021-01-28 12:31:49 -0500

    Update checks.py

--- terminal/checks.py
+++ terminal/checks.py
@@ -5,3 +5,18 @@ import check50.c
 def exists():
     """apt1 exists"""
     check50.exists("apt1/")
+    
+@check50.check()
+def exists():
+    """bathroom exists"""
+    check50.exists("apt1/bathroom/")
+    
+@check50.check()
+def exists():
+    """apt1/bedroom exists"""
+    check50.exists("apt1/bedroom")
+    
+@check50.check()
+def exists():
+    """apt1/kitchen exists"""
+    check50.exists("apt1/kitchen")
