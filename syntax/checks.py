4576433fe895ef9a92acc4830f48e6d32833337f
jmichalenko <50920207+jmichalenko@users.noreply.github.com>
2021-01-29 11:10:16 -0500

    Create checks.py

---
+++ syntax/checks.py
@@ -0,0 +1,17 @@
+import check50
+import check50.c
+
+@check50.check()
+def exists():
+    """Syntax.c exists"""
+    check50.exists("syntax.c")
+
+@check50.check(exists)
+def compiles():
+    """syntax.c compiles"""
+    check50.c.compile("syntax.c", lcs50=True)
+
+@check50.check(compiles)
+def thisIsCS50():
+    """CS50 is displayed"""
+    check50.run("./syntax").stdout("This is CS50!").exit()
