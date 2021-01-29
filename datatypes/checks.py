3b47bd9ad94a9ff3f7e64fcfd77a50246ea4bb68
jmichalenko <50920207+jmichalenko@users.noreply.github.com>
2021-01-29 12:35:48 -0500

    Create checks.py

---
+++ datatypes/checks.py
@@ -0,0 +1,19 @@
+  
+import check50
+import check50.c
+
+@check50.check()
+def exists():
+    """datatypes.c exists"""
+    check50.exists("datatypes.c")
+    check50.include("1.txt")
+
+@check50.check(exists)
+def compiles():
+    """datatypes.c compiles"""
+    check50.c.compile("datatypes.c", lcs50=True)
+
+@check50.check(compiles)
+def test1():
+    """handles inputs correctly"""
+    out = check50.run("./variables").stdin(open("2.txt").read()).stdout(open("1.txt").read())
