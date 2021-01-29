548f7deef0ec09a68b3757cb87d5dd281760b7a9
jmichalenko <50920207+jmichalenko@users.noreply.github.com>
2021-01-29 14:14:18 -0500

    Create checks.py

---
+++ while/checks.py
@@ -0,0 +1,22 @@
+mport check50
+import check50.c
+
+@check50.check()
+def exists():
+    """while.c exists"""
+    check50.exists("while.c")
+
+@check50.check(exists)
+def compiles():
+    """while.c compiles"""
+    check50.c.compile("while.c", lcs50=True)
+
+@check50.check(compiles)
+def eighty():
+    """responds to nuber of 80"""
+    check50.run("./booleans").stdin("80").stdout("Your number can be doubled 2 times before reaching 100!").exit()
+    
+@check50.check(compiles)
+def four():
+    """responds to number of 4"""
+    check50.run("./booleans").stdin("80").stdout("Your number can be doubled 5 times before reaching 100!").exit()
