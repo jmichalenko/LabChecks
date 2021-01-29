ec9102ecaf5e5d647b6ca4e845e1b0b4fb28a830
jmichalenko <50920207+jmichalenko@users.noreply.github.com>
2021-01-29 13:54:10 -0500

    Create checks.py

---
+++ booleans/checks.py
@@ -0,0 +1,32 @@
+import check50
+import check50.c
+
+@check50.check()
+def exists():
+    """booleans.c exists"""
+    check50.exists("booleans.c")
+
+@check50.check(exists)
+def compiles():
+    """boolean.c compiles"""
+    check50.c.compile("booleans.c", lcs50=True)
+
+@check50.check(compiles)
+def A():
+    """responds to grade 90-100"""
+    check50.run("./booleans").stdin("90").stdout("You geta A!").exit()
+
+@check50.check(compiles)
+def B():
+    """responds to grade 80-89"""
+    check50.run("./booleans").stdin("89").stdout("You geta B!").exit()
+    
+@check50.check(compiles)
+def C():
+    """responds to grade 70-79"""
+    check50.run("./booleans").stdin("75").stdout("You geta C!").exit()
+
+@check50.check(compiles)
+def D():
+    """responds to grade below 70"""
+    check50.run("./booleans").stdin("69").stdout("You need to work harder to pass this class").exit()
