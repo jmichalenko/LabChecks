7f5cbe75edc2f7e50a995a0ffce52748d56b0851
jmichalenko <50920207+jmichalenko@users.noreply.github.com>
2021-02-01 11:40:11 -0500

    Update checks.py

--- forloop/checks.py
+++ forloop/checks.py
@@ -3,16 +3,16 @@ import check50.c
 
 @check50.check()
 def exists():
-    """dowhile.c exists"""
+    """forloop.c exists"""
     check50.exists("foorloop.c")
 
 @check50.check(exists)
 def compiles():
-    """dowhile.c compiles"""
+    """forloop.c compiles"""
     check50.c.compile("forloop.c", lcs50=True)
     
 @check50.check(compiles)
-def ten():
+def numbers_add_up_correctly():
     """Adds up the total for numbers 1-10"""
-    check50.run("./forloop").stdout(The total of the numbers from 1 to 10 is: 55).exit()
+    check50.run("./forloop").stdout("The total of the numbers from 1 to 10 is: 55").exit()
     
