12a760781d3a95b21170b61d806f21d0b6812a81
jmichalenko <50920207+jmichalenko@users.noreply.github.com>
2021-02-02 08:35:43 -0500

    Update checks.py

--- exit/checks.py
+++ exit/checks.py
@@ -18,6 +18,11 @@ def incorrect_arguments():
     check50.run("./exit").stdout("Usage: ./exit <firstname> <lastname>").exit()
 
 @check50.check(compiles)
+def incorrect_arguments_one():
+    """responds to only one argument provided"""
+    check50.run("./exit John").stdout("Usage: ./exit <firstname> <lastname>").exit()
+
+@check50.check(compiles)
 def responds_correctly():
     """responds to correct number of arguments"""
     check50.run("./exit John Michalenko").stdout("Hello, John Michalenko!").exit()
