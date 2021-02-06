012c52e3655ea53307cb72d8780ff89d985128b9
jmichalenko <50920207+jmichalenko@users.noreply.github.com>
2021-02-06 10:22:18 -0500

    Create checks.py

---
+++ debugging/checks.py
@@ -0,0 +1,22 @@
+import check50
+import check50.c
+
+@check50.check()
+def exists():
+    """buggy.c exists"""
+    check50.exists("buggy.c")
+
+@check50.check(exists)
+def compiles():
+    """buggy.c compiles"""
+    check50.c.compile("buggy.c", lcs50=True)
+
+@check50.check(compiles)
+def test1():
+    """handles an input of 110 correctly"""
+    out = check50.run("./buggy").stdin("110").stdout("110 in binary is 6 in decimal")
+
+@check50.check(compiles)
+def test2():
+    """rejects non binary number"""
+    out = check50.run("./buggy").stdin("33").rejects()
