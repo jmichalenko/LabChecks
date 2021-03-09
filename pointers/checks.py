84dd3afdead984e29ead133eaf318532bf5530f2
jmichalenko <50920207+jmichalenko@users.noreply.github.com>
2021-03-09 12:28:15 -0500

    Create checks.py

---
+++ pointers/checks.py
@@ -0,0 +1,18 @@
+import check50
+import check50.c
+
+@check50.check()
+def exists():
+    """pointers.c exists"""
+    check50.exists("pointers.c")
+    check50.include("1.txt")
+
+@check50.check(exists)
+def compiles():
+    """pointers.c compiles"""
+    check50.c.compile("pointers.c", lcs50=True)
+    
+@check50.check(compiles)
+def numbers_add_up_correctly():
+    """First line is lower case, second line is uppercase"""
+    out = check50.run("./pointers").stdin(open("john").read()).stdout(open("1.txt").read())
