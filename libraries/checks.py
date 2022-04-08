d3feb6d652eea34710a76c1086070196bb01e8d3
jmichalenko <50920207+jmichalenko@users.noreply.github.com>
2022-04-08 18:52:46 -0400

    Create checks.py

---
+++ libraries/checks.py
@@ -0,0 +1,20 @@
+import check50
+import check50.c
+
+@check50.check()
+def exists():
+    """multiplication.c exists"""
+    check50.exists("multiplication.c")
+    check50.exists("multiplication.h")
+    check50.include("1.txt")
+    check50.include("2.txt")
+
+@check50.check(exists)
+def compiles():
+    """mulitiplication.c compiles"""
+    check50.c.compile("multiplication.c", lcs50=True)
+
+@check50.check(compiles)
+def test1():
+    """handles inputs correctly"""
+    out = check50.run("./multiplication").stdin(open("2.txt").read()).stdout(open("1.txt").read())
